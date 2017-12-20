aa24634574170e4d2eaafcedc4f6f873e36c64da
Leo Leung <leungleoqin@gmail.com>
2017-12-19 21:42:25 -0800

    Refactor solution for isValidBST

--- ctci/4.5_isValidBST.js
+++ ctci/4.5_isValidBST.js
@@ -1,25 +1,11 @@
 export function isValidBST(tree) {
   if (!tree) throw new Error('invalid tree');
 
-  return getBSTInfo(tree.root)[0];
+  const recursiveFunc = (node, min = Number.NEGATIVE_INFINITY, max = Number.POSITIVE_INFINITY) => {
+    if (!node) return true;
+    if (node.val < min || node.val > max) return false;
+    return recursiveFunc(node.left, min, node.val) && recursiveFunc(node.right, node.val, max);
+  };
+
+  return recursiveFunc(tree.root);
 }
-
-const getBSTInfo = (node) => {
-  const result = [true, node.val, node.val];
-
-  if (node.left) {
-    const leftResult = getBSTInfo(node.left);
-    if (!leftResult[0] || leftResult[2] > node.val) result[0] = false;
-    result[1] = leftResult[1];
-  }
-
-  if (!result[0]) return result;
-
-  if (node.right) {
-    const rightResult = getBSTInfo(node.right);
-    if (!rightResult[0] || rightResult[1] < node.val) result[0] = false;
-    result[2] = rightResult[2];
-  }
-
-  return result;
-};
